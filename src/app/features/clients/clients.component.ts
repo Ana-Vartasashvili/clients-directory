5dbd47499be2cbf943bf9bbb451ef106ba73f321
ana.vartasashvili <ana.vartasashvili@europebet.com>
2025-02-08 18:15:54 +0400

    chore: add clients component

---
+++ src/app/features/clients/clients.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-clients',
+  imports: [],
+  templateUrl: './clients.component.html',
+  styleUrl: './clients.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ClientsComponent {
+
+}

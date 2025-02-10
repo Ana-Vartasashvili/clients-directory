78ed2ee2ed4000442a78976e1d646ca1a8f4896a
ana.vartasashvili <ana.vartasashvili@europebet.com>
2025-02-10 22:08:17 +0400

    feat: make cutom input component form control

--- src/app/features/shared/components/input/input.component.ts
+++ src/app/features/shared/components/input/input.component.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core'
+import { ChangeDetectionStrategy, Component, forwardRef, Input } from '@angular/core'
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms'
 import { FloatLabelModule } from 'primeng/floatlabel'
 import { InputTextModule } from 'primeng/inputtext'
 
@@ -7,10 +8,50 @@ import { InputTextModule } from 'primeng/inputtext'
   imports: [FloatLabelModule, InputTextModule],
   templateUrl: './input.component.html',
   styleUrl: './input.component.scss',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputComponent),
+      multi: true,
+    },
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class InputComponent {
+export class InputComponent implements ControlValueAccessor {
   @Input() type: 'text' | 'number' | 'tel' = 'text'
   @Input({ required: true }) name = ''
   @Input({ required: true }) label = ''
+
+  value: string | number = ''
+  disabled: boolean = false
+
+  onChange = (value: string | number) => {}
+  onTouched = () => {}
+
+  writeValue(value: string | number): void {
+    this.value = value
+    this.onChange(value)
+  }
+
+  registerOnChange(fn: (value: string | number) => void): void {
+    this.onChange = fn
+  }
+
+  registerOnTouched(fn: () => void): void {
+    this.onTouched = fn
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    this.disabled = isDisabled
+  }
+
+  onInput(event: Event): void {
+    const input = event.target as HTMLInputElement
+    this.value = input.value
+    this.onChange(this.value)
+  }
+
+  onBlur(): void {
+    this.onTouched()
+  }
 }
